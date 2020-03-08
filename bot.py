54d60a1b78d9a50d0953c40ca20211316093ba16
Kevin Higgs <github@kevinhiggs.com>
2020-03-08 10:16:36 -0400

    updates

--- bot.py
+++ bot.py
@@ -4,30 +4,6 @@ const client = new Discord.Client();
 const http = require('http')
 const server = http.createServer((a,b)=>b.end('ok'))
 server.listen(2900,_=>_)
-const req = o=>o&&o.__esModule?o:{default:o};
-const discordIRC = req(require('discord-irc'));
-(0,discordIRC.default)({
-	"nickname": "blebleblebleble",
-	"server": "irc.freenode.net",
-	"discordToken": process.env.TOKEN,
-	"channelMapping": {
-		"685520418827927612": "#angstromctf"
-	},
-	"format": { // Optional custom formatting options
-		// Patterns, represented by {$patternName}, are replaced when sending messages
-		"commandPrelude": "Message from IRC:", // Message sent before a command
-		"ircText": "<{$displayUsername}> {$text}", // When sending a message to IRC
-		"urlAttachment": "<{$displayUsername}> {$attachmentURL}", // When sending a Discord attachment to IRC
-		"discord": "**<{$author}>** {$withMentions}", // When sending a message to Discord
-		// Other patterns that can be used:
-		// {$discordChannel} (e.g. #general)
-		// {$ircChannel} (e.g. #irc)
-		"webhookAvatarURL": "https://robohash.org/{$nickname}" // Default avatar to use for webhook messages
-	},
-	"webhooks": {
-		"685520418827927612": "https://discordapp.com/api/webhooks/685930097920049182/QapIGe6qYxSxfyGXqW3lArwMbdNIBjCLOmz76L5cDtHlMXX3npNLqEDHs-eFhefHVdfu"
-	}
-});
 let defund = `no more weebs
 burn the weebs
 waw is evil
@@ -46,14 +22,14 @@ function emojify(m){
 }
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
-	let actf = client.guilds.resolve("685493960696791051");
-	let roles = actf.channels.resolve("685528228412260352");
-	roles.messages.fetch("685531046095224842",true);
+	let actf = client.guilds.resolve("686027739966341154");
+	let roles = actf.channels.resolve("686028786508103737");
+	roles.messages.fetch("686029687469637653",true);
 });
 
 client.on('message', msg => {
 	const sender = msg.guild?msg.guild.member(msg.author):null;
-	if (msg.content == "!ping" && msg.channel.id=="685903047381352454") {
+	if (msg.content == "!ping" && msg.channel.id=="686030136406966311") {
 		msg.channel.send("pong");
 	}
 	if (/actf{.+}/.test(msg.content)) {
@@ -69,11 +45,11 @@ client.on('message', msg => {
 		}
 	}
 	
-	if (~msg.content.indexOf('kmh') && msg.channel.id=="685903047381352454") {
+	if (~msg.content.indexOf('kmh') && msg.channel.id=="686030136406966311") {
 		msg.channel.send(emojify(kmh[~~(Math.random()*kmh.length)]));
 	}
 	
-	if (~msg.content.indexOf('defund') && msg.channel.id=="685903047381352454") {
+	if (~msg.content.indexOf('defund') && msg.channel.id=="686030136406966311") {
 		msg.channel.send(emojify(defund[~~(Math.random()*defund.length)]));
 	}
 	
@@ -90,31 +66,30 @@ client.on('messageUpdate', (oldMessage,newMessage) => {
 });
 
 client.on('messageReactionAdd', (reaction,user) => {
-	if (reaction.message.id=="685531046095224842") {
+	if (reaction.message.id=="686029687469637653") {
 		const guild = reaction.message.guild;
 		const sender = guild?guild.member(user):null;
 		const emoji = reaction.emoji;
 		if (emoji.name == '🔔') {
-			sender.roles.add("685537540002152478")
-		}
-		if (emoji.name == '🚩') {
-			sender.roles.add("685529428930854964")
+			sender.roles.add("686030992657612806")
 		}
 	}
 });
 
 client.on('messageReactionRemove', (reaction,user) => {
-	if (reaction.message.id=="685531046095224842") {
+	if (reaction.message.id=="686029687469637653") {
 		const guild = reaction.message.guild;
 		const sender = guild?guild.member(user):null;
 		const emoji = reaction.emoji;
 		if (emoji.name == '🔔') {
-			sender.roles.remove("685537540002152478")
-		}
-		if (emoji.name == '🚩') {
-			sender.roles.remove("685529428930854964")
+			try { sender.roles.remove("686030992657612806") } catch (e) {}
 		}
 	}
 });
+
+client.on('guildMemberAdd', member => {
+	console.log(member)
+	member.roles.add("686028267475697666")
+})
 
 client.login(process.env.TOKEN);
